712eca9d6f72e0824fc2aa6eb6792b4034b8fbc0
batorgiln2016-sketch <batorgil.n2016@orkhonschool.edu.mn>
2026-09-21 09:25:30 +0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -4,16 +4,13 @@
 def count_characters(text):
     return len(text)
 
-
 # Exercise 2
 def remove_spaces(text):
     return text.replace(" ", "")
 
-
 # Exercise 3
 def count_vowels(text):
     return sum(text.lower().count(vowel) for vowel in "aeiou")
-
 
 # Exercise 4
 def replace_vowels(text):
@@ -25,12 +22,10 @@ def replace_vowels(text):
             result += char
     return result
 
-
 # Exercise 5
 def count_words(text):
     words = text.split()
     return len(words)
-
 
 # Exercise 6
 def find_longest_word(text):
