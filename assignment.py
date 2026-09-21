ec3092f92dcb5cbe7d7159ab23a6b65f1e6b58d4
batorgiln2016-sketch <batorgil.n2016@orkhonschool.edu.mn>
2026-09-21 09:05:47 +0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -3,9 +3,11 @@
 # Exercise 1
 def count_characters(text):
     print(len(text))
+    
 # Exercise 2
 def remove_spaces(text):
     print(text.replace(" ", ""))
+    
 # Exercise 3
 def count_vowels(text):
     # Write your code here
